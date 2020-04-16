883c45e4823ac119960ae64a56af4132c7f96b08
Jagannathan <ijaganna@a483e7afa277.ant.amazon.com>
2020-04-16 08:55:42 -0400

    Some more CDK stuff. WIP

--- cdk/pet_stack/bin/pet_stack.ts
+++ cdk/pet_stack/bin/pet_stack.ts
@@ -5,5 +5,5 @@ import { PetGenericresourcesStack } from '../lib/pet_genericresources-stack'
 import {PetStatusUpdater} from '../lib/petstatusupdater'
 
 const app = new cdk.App();
-//new PetGenericresourcesStack(app, 'PetGenericresourcesStack');
+new PetGenericresourcesStack(app, 'PetGenericresourcesStack');
 new PetStatusUpdater(app, 'PetStatusUpdater');
--- cdk/pet_stack/lib/petstatusupdater.ts
+++ cdk/pet_stack/lib/petstatusupdater.ts
@@ -1,28 +1,47 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
-import { Tracing } from '@aws-cdk/aws-lambda';
+import * as iam from '@aws-cdk/aws-iam';
+import { removeListener } from 'cluster';
 
 
 export class PetStatusUpdater extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const petstatusupdater = new lambda.Function(this, 'HelloHandler', {
+    var iamrole = new iam.Role(this, 'lambdaexecutionrole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [iam.ManagedPolicy.fromManagedPolicyArn(this, 'first', 'arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess'),
+      iam.ManagedPolicy.fromManagedPolicyArn(this, 'second', 'arn:aws:iam::aws:policy/AWSLambdaFullAccess')],
+      roleName: 'PetStatusUpdaterRole'
+    });
+
+    var petstatusupdater = new lambda.Function(this, 'lambdafn', {
       runtime: lambda.Runtime.NODEJS_12_X,    // execution environment
       code: lambda.Code.fromAsset('../../petstatusupdater/function.zip'),  // code loaded from "lambda" directory
-      handler: 'exports.handler',
-      tracing: Tracing.ACTIVE,
+      handler: 'index.handler',
+      tracing: lambda.Tracing.ACTIVE,
+      role: iamrole,
       environment:
       {
         "TABLE_NAME": "petadoptions"
       }
     });
 
-    // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, 'PetAdoptionStatusUpdater', {
+
+    // const plain = new apigw.RestApi(this, 'sdfsd', {
+    //   restApiName: 'PetAdoptionStatusUpdater',
+    //   defaultIntegration: new apigw.LambdaIntegration(petstatusupdater, { proxy: false })
+
+    // });
+
+    // defines an API Gateway REST API resource backed by our "petstatusupdater" function.
+    const apigateway = new apigw.LambdaRestApi(this, 'PetAdoptionStatusUpdater', {
       handler: petstatusupdater
     });
+   
 
+
+    //apigateway.methods= [new apigw.Method(this,'sd',{httpMethod:'PUT'})]
   }
 }
