a6113267473adb707d1139b3a042d6f9a3e3c076
Jagannathan <ijaganna@a483e7afa277.ant.amazon.com>
2020-04-27 19:45:18 -0400

    Consolidate into services.ts

--- cdk/pet_stack/bin/pet_stack.ts
+++ cdk/pet_stack/bin/pet_stack.ts
@@ -4,21 +4,21 @@ import * as cdk from '@aws-cdk/core';
 import { PetGenericresourcesStack } from '../lib/pet_genericresources'
 import { PetStatusUpdater } from '../lib/petstatusupdater'
 import { PetVPC } from '../lib/pet_vpc';
-import { PayForAdoption } from '../lib/pet_payforadoption'
+import { PayForAdoption } from '../lib/test_only_pet_payforadoption'
 import { TransactionsDb } from '../lib/pet_transactions_sqldb';
-import { PetListAdoptions } from '../lib/pet_petlistadoption';
-import { PetSearch } from '../lib/pet_petsearch';
-import { PetSite } from '../lib/pet_petsite';
+import { PetListAdoptions } from '../lib/test_only_pet_petlistadoption';
+import { PetSearch } from '../lib/test_only_pet_petsearch';
+import { PetSite } from '../lib/test_only_pet_petsite';
 import { Services } from '../lib/services';
 
 const app = new cdk.App();
 new PetGenericresourcesStack(app, 'PetGenericresourcesStack');
 new PetStatusUpdater(app, 'PetStatusUpdater');
 new PetVPC(app, 'PetVPC');
-new PayForAdoption(app, 'PayForAdoptions');
+// new PayForAdoption(app, 'PayForAdoptions');
 new TransactionsDb(app, 'PetTransactionsDb');
-new PetListAdoptions(app, 'PetListAdoptions');
-new PetSearch(app, 'PetSearch');
-new PetSite(app, 'PetSite');
+// new PetListAdoptions(app, 'PetListAdoptions');
+// new PetSearch(app, 'PetSearch');
+// new PetSite(app, 'PetSite');
 
 new Services(app,'Services');
