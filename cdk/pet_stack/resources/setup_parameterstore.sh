60a8aae8a4b15cf834374f3a9a9854390a34d60c
Jagannathan <ijaganna@a483e7afa277.ant.amazon.com>
2020-05-03 15:06:33 -0400

    Edit parameter store name

--- cdk/pet_stack/resources/setup_parameterstore.sh
+++ cdk/pet_stack/resources/setup_parameterstore.sh
@@ -4,47 +4,47 @@
 
 echo ENTER value for /petstore/cleanupadoptionsurl
 read userinput
-aws ssm put-parameter --name "/clipetstore/cleanupadoptionsurl" --value $userinput"/api/home/cleanupadoptions" --type "SecureString"
+aws ssm put-parameter --name "/petstore/cleanupadoptionsurl" --value $userinput"/api/home/cleanupadoptions" --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/dynamodbtablename
 read userinput
-aws ssm put-parameter --name "/clipetstore/dynamodbtablename" --value $userinput --type "SecureString"
+aws ssm put-parameter --name "/petstore/dynamodbtablename" --value $userinput --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/paymentapiurl
 read userinput
-aws ssm put-parameter --name "/clipetstore/paymentapiurl" --value $userinput"api/home/completeadoption" --type "SecureString"
+aws ssm put-parameter --name "/petstore/paymentapiurl" --value $userinput"api/home/completeadoption" --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/petlistadoptionsurl
 read userinput
-aws ssm put-parameter --name "/clipetstore/petlistadoptionsurl" --value $userinput"/api/adoptionlist/" --type "SecureString"
+aws ssm put-parameter --name "/petstore/petlistadoptionsurl" --value $userinput"/api/adoptionlist/" --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/queueurl
 read userinput
-aws ssm put-parameter --name "/clipetstore/queueurl" --value $userinput --type "SecureString"
+aws ssm put-parameter --name "/petstore/queueurl" --value $userinput --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/s3bucketname
 read userinput
-aws ssm put-parameter --name "/clipetstore/s3bucketname" --value $userinput --type "SecureString"
+aws ssm put-parameter --name "/petstore/s3bucketname" --value $userinput --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/searchapiurl
 read userinput
-aws ssm put-parameter --name "/clipetstore/searchapiurl" --value $userinput"/api/search?" --type "SecureString"
+aws ssm put-parameter --name "/petstore/searchapiurl" --value $userinput"/api/search?" --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/snsarn
 read userinput
-aws ssm put-parameter --name "/clipetstore/snsarn" --value $userinput --type "SecureString"
+aws ssm put-parameter --name "/petstore/snsarn" --value $userinput --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/updateadoptionstatusurl
 read userinput
-aws ssm put-parameter --name "/clipetstore/updateadoptionstatusurl" --value $userinput --type "SecureString"
+aws ssm put-parameter --name "/petstore/updateadoptionstatusurl" --value $userinput --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/rdsconnectionstring
@@ -53,5 +53,5 @@ read rdsServerName
 
 rdsUserName=$(jq .context.rdsusername ../cdk.json)
 rdsPassword=$(jq .context.rdspassword ../cdk.json)
-aws ssm put-parameter --name "/clipetstore/rdsconnectionstring" --value "Server="$rdsServerName";Database=adoptions;User Id="$rdsUserName";Password="$rdsPassword --type "SecureString"
+aws ssm put-parameter --name "/petstore/rdsconnectionstring" --value "Server="$rdsServerName";Database=adoptions;User Id="$rdsUserName";Password="$rdsPassword --type "SecureString"
 echo ------ALL DONE---------
