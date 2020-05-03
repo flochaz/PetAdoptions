3c9baae8f9ebe63ce50d820f4a668d5cb347c4ce
Jagannathan <ijaganna@a483e7afa277.ant.amazon.com>
2020-05-03 15:22:27 -0400

    Add instructions for input values

--- cdk/pet_stack/resources/setup_parameterstore.sh
+++ cdk/pet_stack/resources/setup_parameterstore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-    #Ask the user for their name
-
 echo ENTER value for /petstore/cleanupadoptionsurl
+echo Leave out the 'http://' part. Just enter the domain name. For example, if the ALB URL is http://alb_name.us-east-2.elb.amazonaws.com/, only enter alb_name.us-east-2.elb.amazonaws.com
+
 read userinput
 aws ssm put-parameter --name "/petstore/cleanupadoptionsurl" --value $userinput"/api/home/cleanupadoptions" --type "SecureString"
 echo -------------
@@ -13,16 +13,22 @@ aws ssm put-parameter --name "/petstore/dynamodbtablename" --value $userinput --
 echo -------------
 
 echo ENTER value for /petstore/paymentapiurl
+echo Leave out the 'http://' part. Just enter the domain name. For example, if the ALB URL is http://alb_name.us-east-2.elb.amazonaws.com/, only enter alb_name.us-east-2.elb.amazonaws.com
+
 read userinput
 aws ssm put-parameter --name "/petstore/paymentapiurl" --value $userinput"api/home/completeadoption" --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/petlistadoptionsurl
+echo Leave out the 'http://' part. Just enter the domain name. For example, if the ALB URL is http://alb_name.us-east-2.elb.amazonaws.com/, only enter alb_name.us-east-2.elb.amazonaws.com
+
 read userinput
 aws ssm put-parameter --name "/petstore/petlistadoptionsurl" --value $userinput"/api/adoptionlist/" --type "SecureString"
 echo -------------
 
 echo ENTER value for /petstore/queueurl
+echo Leave out the 'http://' part. Just enter the domain name. For example, if the ALB URL is http://alb_name.us-east-2.elb.amazonaws.com/, only enter alb_name.us-east-2.elb.amazonaws.com
+
 read userinput
 aws ssm put-parameter --name "/petstore/queueurl" --value $userinput --type "SecureString"
 echo -------------
@@ -33,6 +39,7 @@ aws ssm put-parameter --name "/petstore/s3bucketname" --value $userinput --type 
 echo -------------
 
 echo ENTER value for /petstore/searchapiurl
+echo Leave out the 'http://' part. Just enter the domain name. For example, if the ALB URL is http://alb_name.us-east-2.elb.amazonaws.com/, only enter alb_name.us-east-2.elb.amazonaws.com
 read userinput
 aws ssm put-parameter --name "/petstore/searchapiurl" --value $userinput"/api/search?" --type "SecureString"
 echo -------------
@@ -44,6 +51,7 @@ echo -------------
 
 echo ENTER value for /petstore/updateadoptionstatusurl
 read userinput
+echo Leave out the 'http://' part. Just enter the domain name. For example, if the ALB URL is http://alb_name.us-east-2.elb.amazonaws.com/, only enter alb_name.us-east-2.elb.amazonaws.com
 aws ssm put-parameter --name "/petstore/updateadoptionstatusurl" --value $userinput --type "SecureString"
 echo -------------
 
