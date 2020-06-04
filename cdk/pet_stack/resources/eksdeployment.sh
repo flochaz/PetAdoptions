d99a17d52b7b011de7745c1edaf4263021b6c3ae
Imaya Kumar Jagannathan <40274599+awsimaya@users.noreply.github.com>
2020-06-04 10:42:24 -0400

    Read AWS Region from the user

--- cdk/pet_stack/resources/eksdeployment.sh
+++ cdk/pet_stack/resources/eksdeployment.sh
@@ -4,7 +4,12 @@ echo ---------------------------------------------------------------------------
 echo This script deploys petsite service, xray daemon and the CloudWatch agent to the EKS cluster
 echo ---------------------------------------------------------------------------------------------
 
-PETSITE_IMAGE_URL=$(aws cloudformation describe-stacks  --stack-name Services | jq '.Stacks[0].Outputs[] | select(.OutputKey == "imageURLValue").OutputValue')
+# Let the user enter the region
+echo "Enter the AWS Region the EKS cluster has been created (Example: us-east-)"
+read AWS_REGION
+
+# READ Stack name from SSM
+PETSITE_IMAGE_URL=$(aws cloudformation describe-stacks  --stack-name NewecrPetSiteServiceEKS | jq '.Stacks[0].Outputs[] | select(.OutputKey == "petsiteECRimageURL").OutputValue')
 
 sed -i '' "s~{{ECR_IMAGE_URL}}~$PETSITE_IMAGE_URL~" ../../../petsite/petsite/kubernetes/deployment.yaml
 
@@ -13,9 +18,6 @@ kubectl apply -f ../../../petsite/petsite/kubernetes/deployment.yaml
 kubectl apply -f ../../../petsite/petsite/kubernetes/service.yaml
 
 kubectl apply -f ../../../petsite/petsite/kubernetes/xray-daemon/xray-daemon-config.yaml
-
-# Get current AWS REGION to deploy the CW agent
-AWS_REGION=$(aws configure get region)
 
 curl https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/latest/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/quickstart/cwagent-fluentd-quickstart.yaml | sed "s/{{cluster_name}}/petsite/;s/{{region_name}}/$AWS_REGION/" | kubectl apply -f -
 
