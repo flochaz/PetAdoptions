5b4a32fa11b82baee6175442af34ffb4ad9ee893
Imaya Kumar Jagannathan <40274599+awsimaya@users.noreply.github.com>
2020-06-08 10:19:41 -0400

    Remove referencec to EKS-test

--- cdk/pet_stack/app/pet_stack.ts
+++ cdk/pet_stack/app/pet_stack.ts
@@ -3,10 +3,8 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 
 import { Services } from '../lib/services';
-import { PetSiteServiceEKS } from '../lib/eks-test';
 
 const stackName = "Services";
 const app = new cdk.App();
 
 new Services(app, stackName);
-//new PetSiteServiceEKS(app,'ecrPetSiteServiceEKS');
