59298a37b9f4fb600647af63454cda2aaf73a16d
Imaya Kumar Jagannathan <40274599+awsimaya@users.noreply.github.com>
2020-06-09 10:29:23 -0400

    Clean up resources

--- cdk/pet_stack/resources/destroy_stack.sh
+++ cdk/pet_stack/resources/destroy_stack.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 echo ---------------------------------------------------------------------------------------------
-This script destroys the CDK stack
+echo This script destroys the CDK stack
 echo ---------------------------------------------------------------------------------------------
 
+# Fetch the name of the S3 bucket created by CDKToolkit for bootstrap
+CDK_S3_BUCKET_NAME=$(aws cloudformation describe-stacks  --stack-name CDKToolkit | jq '.Stacks[0].Outputs[] | select(.OutputKey == "BucketName").OutputValue')
+
+# Empty the S3 bucket CDKToolkit created
+aws s3 rm s3://$CDK_S3_BUCKET_NAME --recursive   
+
+# Delete resources such as S3 buckets etc createed by CDKToolkit
+aws cloudformation delete-stack --stack-name CDKToolkit
+
+# Get the mail stack name
 STACK_NAME=$(aws ssm get-parameter --name '/petstore/petsiteurl' --region $AWS_REGION | jq .Parameter.Value)
 
-#aws cloudformation delete-stack --stack-name CDKToolkit
-
+# Get rid of all resources
 cdk destory $STACK_NAME
+
+# Sometimes the Sqlseeder doesn't get deleted cleanly. This helps clean up the environment completely including Sqlseeder
+aws cloudformation delete-stack --stack-name $STACK_NAME
+
+echo ----- ✅ DONE --------
