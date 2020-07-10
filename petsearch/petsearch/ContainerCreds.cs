82dfc62dd2e1f0386a4bfd4ea2e77874c041fea7
Imaya Kumar Jagannathan <ijaganna@amazon.com>
2020-07-10 12:54:18 -0400

    SigTest

---
+++ petsearch/petsearch/ContainerCreds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetSearch
+{
+    public class ContainerCreds
+    {
+        public string AccessKeyId { get; set; }
+        public string Expiration { get; set; }
+        public string RoleArn { get; set; }
+        public string SecretAccessKey { get; set; }
+        public string Token { get; set; }
+    }
+}
